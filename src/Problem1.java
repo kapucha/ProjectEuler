f3bde80f99cd6d8c78b46a1e382a459609d98450
Kasia <kaapucha@gmail.com>
2019-02-05 19:43:30 +0100

    Problem1

---
+++ src/Problem1.java
@@ -0,0 +1,20 @@
+
+public class Problem1 {
+
+	private static int getSumOfTheMultiplies3And5(int limit) {
+		int result = 0;
+		for (int i = 3; i < limit; i++) {
+			if (i % 3 == 0) {
+				result += i;
+			} else if (i % 5 == 0) {
+				result += i;
+			}
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(Problem1.getSumOfTheMultiplies3And5(1000));
+	}
+
+}
