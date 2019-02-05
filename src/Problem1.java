6ff4c210bd5c55ca9a91386ef9489e3a737a07d2
Kasia <kaapucha@gmail.com>
2019-02-05 20:13:15 +0100

    Problem1 with stream filter

--- src/Problem1.java
+++ src/Problem1.java
@@ -1,3 +1,4 @@
+import java.util.stream.IntStream;
 
 public class Problem1 {
 
@@ -11,8 +12,17 @@ public class Problem1 {
 		return result;
 	}
 
+	private static int getSumOfMultipliesInRange(int a, int b) {
+		return IntStream.range(a, b).filter(no -> (no % 3 == 0 || no % 5 == 0)).sum();
+	}
+
+	// private static int getSumOfMultipliesInRangeWithMap(int a, int b) {
+	// return IntStream.range(a, b).map(no -> (no % 3 == 0 || no % 5 == 0));
+	// }
+
 	public static void main(String[] args) {
 		System.out.println(Problem1.getSumOfTheMultiplies3And5(1000));
+		System.out.println(Problem1.getSumOfMultipliesInRange(3, 1000));
 	}
 
 }
